8fa13b4a954d05f525f65fe9b302a4ee0672d3f0
Christian Zufferey <christian@zufferey.com>
2026-03-30 12:22:55 +0200

    refact: indiqué comment copier sa clef ssh dans le container afin de pouvoir utiliser Holy Claude avec github

--- install-openssh-server.sh
+++ install-openssh-server.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 #Petit script pour installer facilement openssh-server dans le container docker après que Holy Claude ait démarré
 #ATTENTION: ne support pas l'arrêt du container, donc il faudra le lancer chaque fois que l'on a besoin d'une connexion ssh avec le container !
-#zf260330.1154
+#zf260330.1221
 
 # source: 
 
 echo -e "
 Installation de openssh-server dans le container...
-
 
 "
 
@@ -27,6 +26,16 @@ On peut se connecter dans le container Holy Claude avec ceci:
 
 ssh root@localhost -p 2222
 ssh claude@localhost -p 2222
+
+ou copier sa clef ssh dans le container
+
+scp -P 2222 -r /root/.ssh/ claude@localhost:/home/claude/
+
+après l'avoir générée avec:
+
+ssh-keygen -t ed25519 -N "" -f ~/.ssh/id_ed25519
+
+ATTENTION: pour qu'elle soit valide sur Github, il faudra lui donner la clef publique !
 
 "
 
