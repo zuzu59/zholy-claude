e0a6e499d102c9c4e9cf5a683b12f8118acb2215
Christian Zufferey <christian@zufferey.com>
2026-03-30 11:46:55 +0200

    refact: cette fois-ci cela devrait fonctionner \o/

--- zentry.sh
+++ zentry.sh
@@ -10,13 +10,18 @@ Démarrage de zentry...
 
 "
 
-docker exec -it holyclaude sh -c 'apt update && apt install -y openssh-server && echo "root:tototutu" | chpasswd'
+docker exec -it holyclaude sh -c 'apt update && apt install -y openssh-server locales && sed -i "/fr_CH.UTF-8 UTF-8/s/^# //g" /etc/locale.gen && locale-gen fr_CH.UTF-8 && export LANG=fr_CH.UTF-8 && update-locale LANG=fr_CH.UTF-8'
 
-docker exec -it holyclaude sh -c 'apt install -y locales && sed -i "/fr_CH.UTF-8 UTF-8/s/^# //g" /etc/locale.gen && locale-gen fr_CH.UTF-8 && export LANG=fr_CH.UTF-8 && update-locale LANG=fr_CH.UTF-8'
+#&& echo "root:tototutu" | chpasswd'
+
+docker exec -it holyclaude sh -c 'sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/"  /etc/ssh/sshd_config && echo "root:tototutu" | chpasswd'
+
+
+#docker exec -it holyclaude sh -c 'apt install -y locales && sed -i "/fr_CH.UTF-8 UTF-8/s/^# //g" /etc/locale.gen && locale-gen fr_CH.UTF-8 && export LANG=fr_CH.UTF-8 && update-locale LANG=fr_CH.UTF-8'
 
 
 docker exec -it holyclaude sh -c 'mkdir -p /run/sshd && chmod 0755 /run/sshd && chown root:root /run/sshd'
-docker exec -d holyclaude sh -c '/usr/sbin/sshd -D'
+docker exec -d holyclaude sh -c "pkill sshd; /usr/sbin/sshd -D"
 
 
 
