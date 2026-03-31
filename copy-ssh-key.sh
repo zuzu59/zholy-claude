34ad39a1dc6969111546b1fa48e6a51792ee2ba8
Christian Zufferey <christian@zufferey.com>
2026-03-31 16:19:29 +0200

    refact: pour améliorations de l'utilisation

--- copy-ssh-key.sh
+++ copy-ssh-key.sh
@@ -1,15 +1,34 @@
 #!/usr/bin/env bash
-#Petit script pour copy très facilement sa clef ssh dans le container docker après que Holy Claude ait démarré
-#ATTENTION: ne support pas l'arrêt du container, donc il faudra le lancer chaque fois que l'on a besoin d'une connexion ssh avec le container !
-#zf260330.1224
+#Petit script pour copier très facilement sa clef ssh locale dans le container docker après que Holy Claude ait démarré
+#ATTENTION: ne support pas l'arrêt du container, donc il faudra le lancer chaque fois que l'on a besoin de sa clef ssh dans le container !
+#zf260331.1045
 
 # source: 
 
+
 echo -e "
-Copie de sa clef ssh dans le container...
+
+Avez-vous déjà une clef ssh locale ?
+Si non, faire ceci avant !
+
+ssh-keygen -t ed25519 -N "" -f ~/.ssh/id_ed25519
 
 "
 
+read -p "Voulez-vous continuer avec la copie de votre clef ssh locale dans le container Holy Claude ?"
+
+echo -e "
+Copie de sa clef ssh locale dans le container...
+
+"
+
+echo -e "Copie de sa clef publique pour l'utilisateur root..."
+ssh-copy-id -p 2222 root@localhost
+
+echo -e "Copie de sa clef publique pour l'utilisateur claude..."
+ssh-copy-id -p 2222 claude@localhost
+
+echo -e "Copie de sa clef privée pour l'utilisateur claude..."
 scp -P 2222 -r /root/.ssh/ claude@localhost:/home/claude/
 
 
@@ -23,15 +42,8 @@ On peut se connecter dans le container Holy Claude avec ceci:
 ssh root@localhost -p 2222
 ssh claude@localhost -p 2222
 
-ou copier sa clef ssh dans le container
 
-scp -P 2222 -r /root/.ssh/ claude@localhost:/home/claude/
-
-après l'avoir générée avec:
-
-ssh-keygen -t ed25519 -N "" -f ~/.ssh/id_ed25519
-
-ATTENTION: pour qu'elle soit valide sur Github, il faudra lui donner la clef publique !
+ATTENTION: pour qu'elle soit valide sur Github, il faudra aussi lui donner votre clef publique !
 
 "
 
--- install-openssh-server.sh
+++ install-openssh-server.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 #Petit script pour installer facilement openssh-server dans le container docker après que Holy Claude ait démarré
 #ATTENTION: ne support pas l'arrêt du container, donc il faudra le lancer chaque fois que l'on a besoin d'une connexion ssh avec le container !
-#zf260330.1228
+#zf260331.1059
 
 # source: 
+
+echo -e "
+
+Avez-vous configuré la variable d'environnent pour le password SSH ?
+
+Password actuel: $SSH_PASSWORD
+
+Si non, faire ceci avant !
+
+export SSH_PASSWORD=votre_password_ssh
+
+"
+
+read -p "Voulez-vous continuer l'installation ?"
+
+
 
 echo -e "
 Installation de openssh-server dans le container...
@@ -11,7 +27,11 @@ Installation de openssh-server dans le container...
 "
 
 docker exec -it holyclaude sh -c 'apt update && apt install -y openssh-server locales && sed -i "/fr_CH.UTF-8 UTF-8/s/^# //g" /etc/locale.gen && locale-gen fr_CH.UTF-8 && export LANG=fr_CH.UTF-8 && update-locale LANG=fr_CH.UTF-8'
-docker exec -it holyclaude sh -c 'sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/"  /etc/ssh/sshd_config && echo "root:tototutu" | chpasswd && echo "claude:tototutu" | chpasswd'
+#docker exec -it holyclaude sh -c 'sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/"  /etc/ssh/sshd_config && echo "root:$SSH_PASSWORD" | chpasswd && echo "claude:$SSH_PASSWORD" | chpasswd'
+
+docker exec -it holyclaude sh -c "sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config && echo root:$SSH_PASSWORD | chpasswd && echo claude:$SSH_PASSWORD | chpasswd"
+
+
 docker exec -it holyclaude sh -c 'mkdir -p /run/sshd && chmod 0755 /run/sshd && chown root:root /run/sshd'
 docker exec -d holyclaude sh -c "pkill sshd; /usr/sbin/sshd -D"
 
@@ -27,15 +47,10 @@ On peut se connecter dans le container Holy Claude avec ceci:
 ssh root@localhost -p 2222
 ssh claude@localhost -p 2222
 
-ou copier sa clef ssh dans le container
+ou copier sa clef ssh locale dans le container
 
 ./copy-ssh-key.sh
 
-après l'avoir générée avec:
-
-ssh-keygen -t ed25519 -N "" -f ~/.ssh/id_ed25519
-
-ATTENTION: pour qu'elle soit valide sur Github, il faudra lui donner la clef publique !
 
 "
 
